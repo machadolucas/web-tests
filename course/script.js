6791ad1ea30546a7708e7d197cb761c804064e16
Lucas Machado <vrx_lmachado@uolinc.com>
2015-04-08 17:30:19 -0300

    Course page update

--- course/script.js
+++ course/script.js
@@ -2,6 +2,22 @@
 
 var linkPrefix = "https://uspdigital.usp.br/jupiterweb/obterDisciplina?sgldis=";
 
+initialize();
+
+function initialize() {
+
+	$('.course').on('click', function(){
+		var courseLink = linkPrefix + $(this).attr('data-course-code');
+		window.open(courseLink);
+	});
+
+	$('.course').each(function() {
+		$(this).prop('title',$(this).attr('data-name'));
+	});
+
+	orderOriginal();
+}
+
 function orderOriginal(){
 	
 }
