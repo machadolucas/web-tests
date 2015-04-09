afda08aee3c94ecdb5f6a98a30b76eed9e6536d8
Lucas Machado <vrx_lmachado@uolinc.com>
2015-04-09 10:55:19 -0300

    Course page working

--- course/script.js
+++ course/script.js
@@ -1,9 +1,4 @@
 
-
-var linkPrefix = "https://uspdigital.usp.br/jupiterweb/obterDisciplina?sgldis=";
-
-initialize();
-
 function initialize() {
 
 	$('.course').on('click', function(){
@@ -18,10 +13,68 @@ function initialize() {
 	orderOriginal();
 }
 
+
 function orderOriginal(){
-	
+	for(var s = 1; s <= uniqueOriginSemesters.length; s++){
+		var selection = $("div[class*='course'][data-origin='s"+s+"']");
+		selection.each(function(i){
+			$(this).css('left', (s-1)*boxWidth + 'px');
+			$(this).css('top', i*boxHeight + 'px');
+		});
+	}	
 }
 
+
 function orderExecuted(){
-	
+	for(var s = 1; s <= uniqueDoneSemesters.length; s++){
+		var selection = $("div[class*='course'][data-done='s"+s+"']");
+		selection.each(function(i){
+			$(this).css('left', (s-1)*boxWidth + 'px');
+			$(this).css('top', i*boxHeight + 'px');
+		});
+	}
+	var selection = $("div[class*='course'][data-doing='true']");
+	selection.each(function(i){
+		$(this).css('left', (uniqueDoneSemesters.length)*boxWidth + 'px');
+		$(this).css('top', i*boxHeight + 'px');
+	});
+	var selection = $("div[class*='course']:not([data-done*=s]):not([data-doing='true'])");
+	selection.each(function(i){
+		$(this).css('left', (uniqueDoneSemesters.length+1)*boxWidth + 'px');
+		$(this).css('top', i*boxHeight + 'px');
+	});
 }
+
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+
+var linkPrefix = "https://uspdigital.usp.br/jupiterweb/obterDisciplina?sgldis=";
+
+
+
+var originSemesters = new Array();
+$(".course").each(function(){
+	if($(this).attr('data-origin') != undefined){
+		originSemesters.push($(this).attr('data-origin'));
+	}
+});
+var uniqueOriginSemesters = originSemesters.filter(onlyUnique);
+
+var doneSemesters = new Array();
+$(".course").each(function(){
+	if($(this).attr('data-done') != undefined){
+		doneSemesters.push($(this).attr('data-done'));	
+	}
+});
+var uniqueDoneSemesters = doneSemesters.filter(onlyUnique);
+
+
+
+
+var boxWidth = $('.course').outerWidth(true);
+var boxHeight = $('.course').outerHeight(true);
+
+initialize();
